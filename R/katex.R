5f23aa0dfd96c331a4de82db0b0483877baefacc
Jeroen Ooms <jeroen@berkeley.edu>
2021-07-07 07:10:05 +0000

    version 1.0.0

---
+++ R/katex.R
@@ -0,0 +1,91 @@
+#' Tex math rendering in R
+#'
+#' Converts tex-style math expressions to html and mathml for use in manual pages or
+#' markdown documents.
+#' The conversion is done in R using V8 ("server-side"), hence the resulting fragment can
+#' be inserted into an HTML document without the need for a JavaScript library like MathJax.
+#' Only the [katex.css](https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css)
+#' style file is required in the final html document.
+#' Use [math_to_rd] for embedding math into R documentation (`.rd`) pages.
+#'
+#' By default, [katex_html] returns a mix of HTML for visual rendering and includes
+#' MathML for accessibility. To only get html, pass `output="html"` in the extra options,
+#' see also the [katex documentation](https://katex.org/docs/options.html).
+#' @export
+#' @name katex
+#' @rdname katex
+#' @family katex
+#' @param tex input string with tex math expression.
+#' @param preview open an HTML preview page showing the snipped in the browser
+#' @param displayMode render math in centered 2D layout, similar to `$$` in tex.
+#' Set to `FALSE` to render (non-centered) inline layout for use in text.
+#' For pdf output, this corresponds to the `\deqn{}` and `\eqn{}` macros, see
+#' [WRE 2.6: Mathematics](https://cran.r-project.org/doc/manuals/r-release/R-exts.html#Mathematics)
+#' @param ... additional html rendering options passed to
+#' [katex.render](https://katex.org/docs/options.html)
+#' @param include_css adds the katex css file to the output.
+#' This is only required once per html webpage. Set to `FALSE` if you include css
+#' files into the your html head some other way.
+#' @returns a string with a html/mathml fragment
+#' @examples # Basic examples
+#' html <- katex_html(example_math())
+#' mathml <- katex_mathml(example_math())
+#'
+#' # Example from katex.org homepage:
+#' macros <- list("\\f" = "#1f(#2)")
+#' math <- "\\f\\relax{x} = \\int_{-\\infty}^\\infty \\f\\hat\\xi\\,e^{2 \\pi i \\xi x} \\,d\\xi"
+#' html <- katex_html(math,  macros = macros)
+#' mathml <- katex_mathml(math,  macros = macros)
+katex_html <- function(tex, displayMode = TRUE, ..., include_css = FALSE, preview = interactive()) {
+  html <- katex_render(tex, displayMode = displayMode, ...)
+  if(isTRUE(preview)){
+    tmp <- tempfile(fileext = '.html')
+    writeLines(htmlify(html), tmp, useBytes = TRUE)
+    viewer <- getOption('viewer', utils::browseURL)
+    viewer(tmp)
+  }
+  if(isTRUE(include_css))
+    html <- paste('<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css">', html, sep = '\n')
+  html
+}
+
+#' @export
+#' @rdname katex
+katex_mathml <- function(tex, displayMode = TRUE, ...) {
+  katex_render(tex = tex, displayMode = displayMode, output = 'mathml', ...)
+}
+
+#' @export
+#' @rdname katex
+example_math <- function(){
+  return("f(x)= {\\frac{1}{\\sigma\\sqrt{2\\pi}}}e^{- {\\frac {1}{2}} (\\frac {x-\\mu}{\\sigma})^2}")
+}
+
+#' @importFrom V8 v8
+.onLoad <- function(lib, pkg){
+  assign("ctx", V8::v8("window"), environment(.onLoad))
+  ctx$source(system.file("js/katex.min.js", package = pkg))
+}
+
+katex_render <- function(tex, displayMode, ...) {
+  options <- list(...)
+  options$displayMode <- displayMode
+  html <- ctx$call('katex.renderToString', tex, options)
+  Encoding(html) = 'UTF-8'
+  html
+}
+
+htmlify <- function(snippet){
+  sub('{{math}}', snippet, template, fixed = TRUE)
+}
+
+template <- '<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css">
+  </head>
+  <body>
+  {{math}}
+  </body>
+</html>'
---
+++ R/rd.R
@@ -0,0 +1,54 @@
+#' Display math in R documentation
+#'
+#' Helper function to insert tex math expressions into R documentation (`.rd`) files.
+#' Uses Katex rendering for documentation in html format, and the appropriate latex
+#' macros for documentation rendered in pdf or plain-text.
+#'
+#' Use `math_to_rd()` inside `\Sexpr` to embed math in your R package documentation
+#' pages. For example the code below can be inserted in your `rd` (or roxygen)
+#' source code:
+#'
+#' ```
+#' \Sexpr[results=rd, stage=build]{
+#'   katex::math_to_rd(katex::example_math())
+#' }
+#' ```
+#'
+#' Which results in the following output:
+#'
+#' \Sexpr[results=rd, stage=build]{
+#'   katex::math_to_rd(katex::example_math())
+#' }
+#'
+#' Optionally you can specify an alternate ascii representation that will be shown in
+#' the plain-text format rendering of the documentation:
+#'
+#' ```
+#' \Sexpr[results=rd, stage=build]{
+#'   katex::math_to_rd('E=MC^2', 'E=mc²')
+#' }
+#' ```
+#'
+#' \Sexpr[results=rd, stage=build]{
+#'   katex::math_to_rd('E=MC^2', 'E=MC²')
+#' }
+#'
+#' If no ascii representation is given, the input tex in displayed verbatim into the
+#' plain-text documentation.
+#'
+#' @export
+#' @name math_to_rd
+#' @rdname math_to_rd
+#' @family katex
+#' @inheritParams katex
+#' @returns a string with an rd fragment to be included in R documentation
+#' @param ascii alternate text-only representation of the input math to show in
+#' documentation rendered to plain text format.
+math_to_rd <- function(tex, ascii = tex, displayMode = TRUE, ..., include_css = TRUE){
+  html <- katex_html(tex, include_css = include_css, displayMode = displayMode, ...,
+                     preview = FALSE)
+  html_out <- paste('\\if{html}{\\out{', html, '}}', sep = '\n')
+  latex_out <- paste('\\if{latex,text}{', ifelse(displayMode, '\\deqn{', '\\eqn{'),
+                     tex, '}{', ascii, '}}', sep = '\n')
+  paste(html_out, latex_out, sep = '\n')
+}
